c3b65984b7e17dad680ed2fb6a2493b8bb743717
Hugo VERJUS <hugo.verjus@epita.fr>
2019-02-21 12:27:14 +0100

    add: vector3d test

--- pathtracer_tests/main.cc
+++ pathtracer_tests/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+
 int main() {
     testing::InitGoogleTest();
     return RUN_ALL_TESTS();
---
+++ pathtracer_tests/tests/vector3dTest.cc
@@ -0,0 +1,151 @@
+
+#include "gtest/gtest.h"
+
+#include "Vector3D.hh"
+
+
+TEST(TestVector3, constructorEquals) {
+    auto a = Vector3D(0, 0, 0);
+    auto b = Vector3D<int>();
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, plusVector) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    a = a + a;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, plusElementRight) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    a = a + 1;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, plusElementLeft) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    a = 1 + a;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, plusEqualVector) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    a += a;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, plusEqualElement) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    a += 1;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, minusVector) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    b = b - a;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, minusElement) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    b = b - 1;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, minusEqualVector) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    b -= a;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, minusEqualElement) {
+    auto a = Vector3D(1, 1, 1);
+    auto b = Vector3D(2, 2, 2);
+    b -= 1;
+    EXPECT_EQ(a, b);
+}
+
+TEST(TestVector3, timesVector) {
+    auto a = Vector3D(2, 2, 2);
+    auto b = Vector3D(2, 2, 2);
+    auto c = Vector3D(4, 4, 4);
+    EXPECT_EQ(a * b, c);
+}
+
+TEST(TestVector3, timesElementLeft) {
+    auto a = Vector3D(2, 2, 2);
+    auto c = Vector3D(4, 4, 4);
+    EXPECT_EQ(2 * a, c);
+}
+
+TEST(TestVector3, timesElementRight) {
+    auto a = Vector3D(2, 2, 2);
+    auto c = Vector3D(4, 4, 4);
+    EXPECT_EQ(a * 2, c);
+}
+
+TEST(TestVector3, timesEqualVector) {
+    auto a = Vector3D(2, 2, 2);
+    auto b = Vector3D(2, 2, 2);
+    auto c = Vector3D(4, 4, 4);
+    a *= b;
+    EXPECT_EQ(a, c);
+}
+
+TEST(TestVector3, timesEqualElement) {
+    auto a = Vector3D(2, 2, 2);
+    auto c = Vector3D(4, 4, 4);
+    a *= 2;
+    EXPECT_EQ(a, c);
+}
+
+
+TEST(TestVector3, divideVector) {
+    auto a = Vector3D(2, 2, 2);
+    auto b = Vector3D(2, 2, 2);
+    auto c = Vector3D(1, 1, 1);
+    EXPECT_EQ(a / b, c);
+}
+
+TEST(TestVector3, divideElement) {
+    auto a = Vector3D(2, 2, 2);
+    auto c = Vector3D(1, 1, 1);
+    EXPECT_EQ(a / 2, c);
+}
+
+
+TEST(TestVector3, divideEqualVector) {
+    auto a = Vector3D(2, 2, 2);
+    auto b = Vector3D(2, 2, 2);
+    auto c = Vector3D(1, 1, 1);
+    a /= b;
+    EXPECT_EQ(a, c);
+}
+
+TEST(TestVector3, divideEqualElement) {
+    auto a = Vector3D(2, 2, 2);
+    auto c = Vector3D(1, 1, 1);
+    a /= 2;
+    EXPECT_EQ(a, c);
+}
+
+TEST(TestVector3, dotProduct) {
+    auto a = Vector3D(1, 2, 3);
+    auto b = Vector3D(2, 4, 6);
+    EXPECT_EQ(a.dotproduct(b), 28);
+}
+
+TEST(TestVector3, crossProduct) {
+    auto a = Vector3D(1, 2, 3);
+    auto b = Vector3D(6, 5, 7);
+    EXPECT_EQ(a.crossproduct(b), Vector3D(-1, 11, -7));
+}
+
