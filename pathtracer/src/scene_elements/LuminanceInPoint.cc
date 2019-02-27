b525a50c8174fb6348688a74934fed6965813d80
marion <marion.claus@epita.fr>
2019-02-27 19:33:54 +0100

    Find the shadow ray for one point

--- pathtracer/src/scene_elements/LuminanceInPoint.cc
+++ pathtracer/src/scene_elements/LuminanceInPoint.cc
@@ -3,14 +3,50 @@
 //
 
 #include "LuminanceInPoint.hh"
-#include "Ray.hh"
 
-bool LuminanceInPoint::shadow_ray(Vector3D<float> p1, Vector3D<float> p2)
+bool LuminanceInPoint::IsShadowRay(Vector3D<float> p1, Vector3D<float> p2)
 {
-    Vector3D<float> direction = p1 - p2;
-    for (auto it = objects_.begin(); it != objects_.end(); it++)
-    {
-        Ray shadow = Ray(p1, direction);
+    Vector3D<float> direction = p2 - p1;
+    Ray ray = Ray(p1, direction);
 
+    std::vector<Vector3D<float>> intersections = ray.intersectAllObjects(objects_);
+    //Compute distance(p1, intersection) < distance(p1, p2) ==> False there is no shadow_ray between p1 and p2
+    for (const auto &intersection : intersections)
+    {
+        auto distance_p1_intersection = (intersection - p1).norm();
+        auto distance_p1_p2 = direction.norm();
+        if (distance_p1_intersection < distance_p1_p2)
+            return false;
+    }
+    return true;
+}
+
+
+std::vector<Vector3D<float>> LuminanceInPoint::allShadowRays(Vector3D<float> p)
+{
+    std::vector<Vector3D<float>> shadow_ray;
+    for (const auto &intersectPointLight : ray_light_)
+    {
+        if (this->IsShadowRay(p, intersectPointLight))
+            shadow_ray.push_back(intersectPointLight);
+    }
+    return shadow_ray;
+}
+
+
+void LuminanceInPoint::luminanceOnePoint(Vector3D<float> p)
+{
+    std::vector<Vector3D<float>> shadow_ray = this->allShadowRays(p);
+    //Compute Luminance of p
+    /*FIXME*/
+}
+
+
+void LuminanceInPoint::luminanceAllPoint()
+{
+    for (auto it = ray_viewer_.rbegin(); it != ray_viewer_.rend(); it++)
+    {
+        this->luminanceOnePoint(*it);
+        /*FIXME*/
     }
 }
--- pathtracer/src/scene_elements/LuminanceInPoint.hh
+++ pathtracer/src/scene_elements/LuminanceInPoint.hh
@@ -2,24 +2,27 @@
 // Created by claus on 26/02/2019.
 //
 
-#ifndef PATHTRACER_LUMINANCEINPOINT_HH
-#define PATHTRACER_LUMINANCEINPOINT_HH
+#pragma once
 
 #include <vector>
+
 #include "fixed_size_vectors/Vector3D.hh"
 #include "Polygon.hh"
+#include "Ray.hh"
 
 class LuminanceInPoint {
 public:
-    bool shadow_ray(Vector3D<float> p1, Vector3D<float> p2);
-    float luminance_in_point(Vector3D<float> p1);
-    float luminance();
+    //Verify that there is no objects between the point p1 and the point p2
+    bool IsShadowRay(Vector3D<float> p1, Vector3D<float> p2);
+    //Return all the shadow ray of point p
+    std::vector<Vector3D<float>> allShadowRays(Vector3D<float> p);
+    //Compute luminance on point p with the shadow ray
+    void luminanceOnePoint(Vector3D<float> p); //call AllShadowRays for point p and compute is luminance
+    //Compute luminance in all viewer intersection point
+    void luminanceAllPoint();
 
 private:
     std::vector<Vector3D<float>> ray_viewer_;
     std::vector<Vector3D<float>> ray_light_;
     std::vector<Polygon> objects_;
 };
-
-
-#endif //PATHTRACER_LUMINANCEINPOINT_HH
